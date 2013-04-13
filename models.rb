d4898b5e0bc88b2e2c818ee166a985f88f809149
Steve Kinney <hello@stevekinney.net>
2013-04-13 16:22:09 -0400

    Add my session in

--- models.rb
+++ models.rb
@@ -30,6 +30,7 @@ class Session
       "Developing iLearn Coursework/Blended Learning",
       "iPads in the Classroom ",
       "SMART Boards and the Common Core"
+      "Technology Grab Bag: Be More Effecient and Effective with Technology"
     ].each do |session|
       Session.create({title: session})
     end
